772f72bc0618cdcd716421f8c9f0380519b3a3ed
callor <callor@callor.com>
2022-07-12 14:52:15 +0900

    Food

--- node-310-Food/client/src/comps/DietInput.jsx
+++ node-310-Food/client/src/comps/DietInput.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 import uuid from "react-uuid";
 import moment from "moment";
 
-const DietInput = () => {
+const DietInput = ({ params }) => {
+  // params 로 전달받은 데이터와 함수중에서 필요한 부분만 분해하여
+  // 변수에 받기
+  const { fetchFood, setFoods } = params;
+
   // JSON type 의 food state 변수 생성
   const [food, setFood] = useState({
     d_id: uuid(),
@@ -23,7 +27,6 @@ const DietInput = () => {
     name 변수와 value 변수가 분해되고 각각의 별도 변수로 생성된다
     각각 별도 변수로 선언된 속성을 사용하여 
     food JSON  객체에 값을 담는다
-
     */
     const { name, value } = e.target;
     console.log(value);
@@ -36,12 +39,12 @@ const DietInput = () => {
   // 현재 JS 버전에서는 하나의 event 핸들러를 통하여
   // 사용할 수 있다.
   // { [name] : 값 }
-  const onDateChange = (e) => {
-    setFood({ ...food, d_date: e.target.value });
-  };
-  const onFoodChange = (e) => {
-    setFood({ ...food, d_food: e.target.value });
-  };
+  // const onDateChange = (e) => {
+  //   setFood({ ...food, d_date: e.target.value });
+  // };
+  // const onFoodChange = (e) => {
+  //   setFood({ ...food, d_food: e.target.value });
+  // };
 
   const onClick = async (e) => {
     console.log(food);
@@ -52,11 +55,37 @@ const DietInput = () => {
       },
       body: JSON.stringify(food),
     };
-    const res = await fetch("http://localhost:3000/food/insert", postOption);
+    /*
+    react 와 node 사이에서 데이터를 주고받는데
+    서로 다른 PORT 통해서 데이터를 주고 받게 되어
+    CORS(Cross Origin Resouce Share, 교차사이트 스크립트 오류)가
+    발생을 한다.
+    서버와 서버간의 통신(데이터 교환)이 이루어질때
+    보안, 해캥 방지 등을 이유로 점점 많은곳에서 스크립트 교환이
+    이루어지지 않도록 하고 있다
+    CORS 때문에 실제 필요한 API 이용에 제한 많다
+    React, NodeJS 가 같은 서버에서 작동될때는 Package.json 에
+    proxy 설정을 통하여 CORS 문제를 일부 해결 할 수 있다.
+
+    proxy 설정을 했을 경우는 fetch URL 부분에 http://localhost:3000 주소를
+    생략하고 router 주소만 사용하여 nodejs 와 데이터를 주고 받는다
+    */
+    const res = await fetch("/food/insert", postOption);
     if (res.ok) {
       const json = await res.json();
       console.log(json);
+      fetchFood().then((result) => {
+        setFoods(result);
+      });
     }
+
+    setFood({
+      d_id: uuid(),
+      d_date: moment().format("YYYY[-]MM[-]DD"),
+      d_food: "",
+      d_qty: "",
+      d_cal: "",
+    });
   };
 
   return (
@@ -66,7 +95,7 @@ const DietInput = () => {
           type="date"
           name="d_date"
           className="w3-input"
-          defaultValue={food.d_date}
+          value={food.d_date}
           onChange={onChange}
         />
       </div>
@@ -76,6 +105,7 @@ const DietInput = () => {
           name="d_food"
           className="w3-input"
           placeholder="메뉴를 입력하세요"
+          value={food.d_food}
           onChange={onChange}
         />
       </div>
@@ -85,6 +115,7 @@ const DietInput = () => {
           name="d_qty"
           className="w3-input"
           placeholder="섭취수량을 입력하세요"
+          value={food.d_qty}
           onChange={onChange}
         />
       </div>
@@ -94,6 +125,7 @@ const DietInput = () => {
           name="d_cal"
           className="w3-input"
           placeholder="단위 칼로리를 입력하세요"
+          value={food.d_cal}
           onChange={onChange}
         />
       </div>
--- node-310-Food/routes/food.js
+++ node-310-Food/routes/food.js
@@ -3,12 +3,45 @@ const router = express.Router();
 
 import { food_model } from "../config/mongoConfig.js";
 
+router.get("/selectAll", (req, res) => {
+  food_model.find({}).then((result) => res.json(result));
+});
+
 // POST localhost:3000/food/insert
 router.post("/insert", (req, res) => {
   console.log(req.body);
   food_model.create(req.body);
-
   res.json({ OK: "OK" });
 });
 
+/*
+REST Ful 방식의 delete RequestMathod 로 요청을 받고
+주소창에 전달된 값을 id params 변수에 받아 확인하기
+*/
+router.delete("/remove/:id", (req, res) => {
+  const id = req.params.id;
+  console.log("받은 ID", id);
+  /*
+  mongoose 모델의 findOneAndDelete 함수를 호출하여
+  d_id 값이 id 와 일치하는 데이터를 삭제하라
+  findOneAndRemove : findOnAndModify 로 변경되었다
+  실제로 삭제하려면 findOneAndDelete
+  */
+  food_model
+    .findOneAndDelete({ d_id: id })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.send("OK");
+    });
+});
+
 export default router;
+/*
+REST Ful  방식의 요청
+  router, Controller 에서 사용하는 RequestMethod
+    get : 데이터를 요청할때
+    post : 데이터를 추가할때
+    delete : 데이터를 삭제할때
+    put : 데이터를 업데이트 할때
+*/
