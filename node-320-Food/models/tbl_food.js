5f0bb35287d2b1ddeeb0a428d3b04b6e616caee6
callor <callor@callor.com>
2022-07-13 12:44:09 +0900

    mysql_연동

--- node-320-Food/models/tbl_food.js
+++ node-320-Food/models/tbl_food.js
@@ -1,5 +1,5 @@
 import _sequelize from "sequelize";
-const { Model, Sequelize } = _sequelize;
+const { Model } = _sequelize;
 
 export default class tbl_ems extends Model {
   static init(sequelize, DataTypes) {
